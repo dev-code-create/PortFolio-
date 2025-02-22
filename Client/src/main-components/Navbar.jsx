e8f874d664f7e36f3fabdc18c88911a19a348c9e
dev-code-create <ayushkankale04@gmail.com>
2025-02-22 13:40:38 +0530

    Glitch fixed

--- Client/src/main-components/Navbar.jsx
+++ Client/src/main-components/Navbar.jsx
@@ -44,9 +44,9 @@ const navItems = [
   },
 ];
 
-export default function Navbar() {
+export default function FloatingNavbar() {
   return (
-    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50 ">
+    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
       <motion.div
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
@@ -54,26 +54,24 @@ export default function Navbar() {
         className="flex bg-black bg-opacity-80 backdrop-blur-lg px-4 py-3 rounded-full space-x-4 shadow-lg"
       >
         {navItems.map((item) => (
-          <motion.a
+          <div
             key={item.id}
-            href={item.link}
-            className="relative group flex items-center justify-center w-12 h-12 rounded-full bg-gray-800 hover:bg-gray-700 text-white"
-            whileHover={{ scale: 1.2, opacity: 1, y: -5 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ duratiom: 0.2 }}
+            className="relative flex flex-col items-center group"
           >
-            {item.icon}
-            {/* Tooltip */}
-            <motion.span
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 0 }}
-              whileHover={{ opacity: 1, y: -1 }}
+            <motion.a
+              href={item.link}
+              className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-800 hover:bg-gray-700 text-white"
+              whileHover={{ scale: 1.2, opacity: 1, y: -5 }}
+              whileTap={{ scale: 0.9 }}
               transition={{ duration: 0.2 }}
-              className="absolute  bg-red-600 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 "
             >
+              {item.icon}
+            </motion.a>
+            {/* Tooltip now fully inside the hover area */}
+            <span className="absolute bottom-16 flex items-center justify-center bg-red-600 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 ">
               {item.label}
-            </motion.span>
-          </motion.a>
+            </span>
+          </div>
         ))}
       </motion.div>
     </div>
